b7f38ebb1ffd18ffdff34286511b28371d6b7874
Bryan Urizar <bryan.urizar@outlook.com>
2020-10-25 20:02:13 +0000

    Update app.js / index.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-let dailyTemps = []; 
+let dailyTemps = [];
 let dates = [];
 let iconURLS = [];
 let descriptions = [];
